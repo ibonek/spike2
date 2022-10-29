06945c404498c7362ef56b6d2d329b4a6d4bbd39
al394756 <al394756@uji.es>
2022-10-29 19:12:14 +0200

    ticketmaster api

--- src/app/prueba-api/prueba-api.component.ts
+++ src/app/prueba-api/prueba-api.component.ts
@@ -11,20 +11,34 @@ export class PruebaApiComponent implements OnInit {
 
 
   ngOnInit(): void {
-    let inputVal = 'London';
-    const apiKey = '4d8fb5b93d4af21d66a2948710284366';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    let inputVal = 'Madrid';
+    const apiWeatherKey = '4d8fb5b93d4af21d66a2948710284366';
+    const apiTicketKey = '0F9NRxxNHHARWvlAgkokiKrKFYVqEqM8';
+    let contador = 0;
+    let urlTicket = `https://app.ticketmaster.com/discovery/v2/events?apikey=${apiTicketKey}&locale=*&size=6&page=${contador}&sort=relevance,desc&city=${inputVal}`;
+    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiWeatherKey}&units=metric`;
 
-    fetch(url)
+    fetch(urlWeather)
         .then(response => response.json())
         .then(data => {
           const {main, name, sys, weather} = data;
-          const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-              weather[0]["icon"]
-          }.svg`;
           console.log(weather[0].description+" "+main.temp);
         });
 
+    fetch(urlTicket)
+        .then(response => response.json())
+        .then(data => {
+            const {_embedded}=data;
+            console.log(_embedded.events[0].name);
+        })
+      contador++;
+      urlTicket=`https://app.ticketmaster.com/discovery/v2/events?apikey=${apiTicketKey}&locale=*&size=6&page=${contador}&sort=relevance,desc&city=${inputVal}`;
+      fetch(urlTicket)
+          .then(response => response.json())
+          .then(data => {
+              const {_embedded}=data;
+              console.log(_embedded.events[0].name);
+          })
   }
 
 }
